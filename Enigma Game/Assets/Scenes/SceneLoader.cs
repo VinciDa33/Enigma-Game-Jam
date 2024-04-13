411fa83dfb5f722e66a139dc3c2b2bb6c4c46ec7
Marcus <marcusbthomsen@gmail.com>
2024-04-13 14:50:02 +0200

    TMP Import

---
+++ Enigma Game/Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static GameObject instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = gameObject;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadNextScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCount)
+            LoadMenu();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
